bba66da102151e4bcecc2d608d7d0f54b29b57b4
jliliflora <syon704@gmail.com>
2023-06-05 20:02:53 +0900

    theme

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App";
-import { ThemeProvider } from "styled-components";
-import { theme } from "./theme";
 
 const queryClient = new QueryClient();
 
@@ -12,9 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </QueryClientProvider>
 );
 
